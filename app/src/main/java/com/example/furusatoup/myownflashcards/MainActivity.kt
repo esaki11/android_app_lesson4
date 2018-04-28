a2ec19ebec35ddad3b632ab76d2bcfaafd387892
江崎英太 <furusatoup@furusatoUp.local>
2018-04-29 02:14:34 +0900

    WordListActivityボタン類の実装

--- app/src/main/java/com/example/furusatoup/myownflashcards/MainActivity.kt
+++ app/src/main/java/com/example/furusatoup/myownflashcards/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+var intBackGroundColor = 0
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,7 @@ class MainActivity : AppCompatActivity() {
 
 //        「色」ボタンを押した場合
 //        画面の背景色をボタンの色に変更
-<<<<<<< HEAD
-        var intBackGroundColor = 0
+
         buttonColor01.setOnClickListener {
             intBackGroundColor = R.color.color01
             constraintLaioutMain.setBackgroundResource(intBackGroundColor)
@@ -47,13 +48,6 @@ class MainActivity : AppCompatActivity() {
             intBackGroundColor = R.color.color06
             constraintLaioutMain.setBackgroundResource(intBackGroundColor)
         }
-=======
-        buttonColor01.setOnClickListener {  }
-        buttonColor02.setOnClickListener {  }
-        buttonColor03.setOnClickListener {  }
-        buttonColor04.setOnClickListener {  }
-        buttonColor05.setOnClickListener {  }
-        buttonColor06.setOnClickListener {  }
->>>>>>> 2eba7c3eb64756c46d57773bac05bac8084e1959
+
     }
 }
--- app/src/main/java/com/example/furusatoup/myownflashcards/WordListActivity.kt
+++ app/src/main/java/com/example/furusatoup/myownflashcards/WordListActivity.kt
@@ -1,12 +1,28 @@
 package com.example.furusatoup.myownflashcards
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_word_list.*
 
 class WordListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_list)
+
+//        前画面で設定した背景色を設定
+        constraintLaioutWordlist.setBackgroundResource(intBackGroundColor)
+//新しい単語を追加ボタンを押した場合EditActivityへ
+        buttonAddNewWord.setOnClickListener {
+            val intent = Intent(this@WordListActivity, EditActivity::class.java)
+            intent.putExtra(getString(R.string.intent_key_status),getString(R.string.status_add))
+            startActivity(intent)
+        }
+//        戻るボタンを押した場合
+        buttonBack.setOnClickListener {
+            finish()
+        }
     }
 }
